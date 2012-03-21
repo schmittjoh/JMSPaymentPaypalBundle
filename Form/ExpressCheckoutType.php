77d0c23a07f5b7132071c0c8c3d3debdfa034eb9
Johannes M. Schmitt <schmittjoh@gmail.com>
2012-03-20 21:13:23 -0600

    added form type

---
+++ Form/ExpressCheckoutType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JMS\Payment\PaypalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+/**
+ * Type for Paypal Express Checkout.
+ *
+ * @author Johannes M. Schmitt <schmittjoh@gmail.com>
+ */
+class ExpressCheckoutType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+    }
+
+    public function getName()
+    {
+        return 'paypal_express_checkout';
+    }
+}
